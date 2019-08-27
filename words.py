2171f0929cdf7a7dd75d4ab7035059f40da61d07
tafan <tao.1.fan@nokia-sbell.com>
2019-08-27 17:28:11 +0800

    refactor for generating cloud words

--- words.py
+++ words.py
@@ -6,30 +6,34 @@ import os
 import jieba
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, ImageColorGenerator
+from wordcloud import WordCloud, STOPWORDS
 
 
 CURDIR = os.path.abspath(os.path.dirname(__file__))
-PICTURE = os.path.join(CURDIR, 'data', 'girl.png')
-COMMENTS = os.path.join(CURDIR, 'data', 'comments.txt')
+TEXT = os.path.join(CURDIR,  'data', 'comments.txt')
+PICTURE= os.path.join(CURDIR,  'data', 'alice.png')
 FONT = os.path.join(CURDIR, 'data', 'Songti.ttc')
-CLOUD = os.path.join(CURDIR, 'data', 'words.png')
 
 
-def cut_the_words(path):
-    with open(path, 'r') as rp:
+def cut_the_words(test=TEXT):
+    with open(test, 'r') as rp:
         content = rp.read()
     words_list = jieba.cut(content, cut_all = True)
     return ' '.join(words_list)
 
-def create_worlds_cloud(picture, data, font):
-    background = np.array(Image.open(picture))
-    words = cut_the_words(data)
-    wc = WordCloud(background_color="white", mask=background, font_path=font)
+
+def create_worlds_cloud():
+    background = np.array(Image.open(PICTURE))
+    stopwords = set(STOPWORDS)
+    for item in ["上海堡垒", "上海", "堡垒"]:
+        stopwords.add(item)
+    words = cut_the_words()
+    wc = WordCloud(background_color="white",
+                   mask=background,
+                   stopwords=stopwords,
+                   font_path=FONT)
     wc.generate(words)
-    wc.to_file(CLOUD)
+    wc.to_file('data/girl.png')
 
 if __name__ == '__main__':
-    create_worlds_cloud(picture=PICTURE, data=COMMENTS, font=FONT)
-
+    create_worlds_cloud()
